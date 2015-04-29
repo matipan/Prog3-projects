858fb1df8cbe2d896655153b13b8c03c2dd95bff
kriox26 <matias.pan26@gmail.com>
2015-04-29 16:20:43 -0300

    Initial commit for practica7

---
+++ Practica7/src/prog3/heap/Heap.java
@@ -0,0 +1,23 @@
+package prog3.heap;
+
+/**
+ * @author Matias
+ *
+ */
+public class Heap<T extends Comparable<T>> {
+	private T[] datos;
+	private int cantElementos;
+	
+	public Heap(){
+	}
+	
+	public Heap(T[] arreglo){
+		for (int i = 0; i < arreglo.length; i++) {
+			for (int j = i+1; j < arreglo.length; j++) {
+				if(//comparar los datos del arreglo para agarrar el menor) {
+					
+				}
+			}
+		}
+	}
+}
