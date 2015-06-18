6900acbbfcf9e3be62408d2ff7bed9315cce2395
kriox26 <matias.pan26@gmail.com>
2015-06-18 10:52:32 -0300

    BFS implementado

--- Grafos/src/prog3/grafos/utiles/Recorridos.java
+++ Grafos/src/prog3/grafos/utiles/Recorridos.java
@@ -2,6 +2,7 @@ package prog3.grafos.utiles;
 import prog3.grafos.*;
 import prog3.lista.ListaEnlazadaGenerica;
 import prog3.lista.ListaGenerica;
+import prog3.util.ColaGenerica;
 
 /**
  * @author Matias
@@ -37,6 +38,27 @@ public class Recorridos<T> {
 				dfs_rec(grafo,arista.verticeDestino(), visitados, lista_vertices);
 			}
 		}
+	}
+
+	public ListaGenerica<Vertice<T>> bfs(Grafo<T> grafo){
+		ListaGenerica<Vertice<T>> lista_vertices = new ListaEnlazadaGenerica<Vertice<T>>();
+		ColaGenerica<Vertice<T>> cola = new ColaGenerica<Vertice<T>>();
+		boolean[] visitados = new boolean[grafo.listaDeVertices().tamanio()];
+		cola.encolar(grafo.vertice(0));
+		while (!cola.esVacia()) {
+			Vertice<T> tmp = cola.desencolar();
+			ListaGenerica<Arista<T>> adyacentes = grafo.listaDeAdyacentes(tmp);
+			adyacentes.comenzar();
+			while (!adyacentes.fin()) {
+				Arista<T> act = adyacentes.proximo();
+				if (!visitados[act.verticeDestino().posicion()]) {
+					cola.encolar(act.verticeDestino());
+					lista_vertices.agregarInicio(act.verticeDestino());
+					visitados[act.verticeDestino().posicion()] = true;
+				}
+			}
+		}
+		return lista_vertices;
 	}
 
 	public static void main(String[] args) {
